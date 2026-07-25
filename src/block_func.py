218178d0ff872cc770ad9f419c93263feb929cf2
incrediblemet <incrediblemet@gmail.com>
2026-07-25 15:21:49 +1000

    M: Create Markdown Block to BlockType function and add unit tests

--- src/block_func.py
+++ src/block_func.py
@@ -1,4 +1,5 @@
-
+from enum import Enum
+import re
 
 def markdown_to_blocks(markdown):
     blocks_list = markdown.split("\n\n")
@@ -7,3 +8,25 @@ def markdown_to_blocks(markdown):
         blocks.append(block.strip())
     blocks = [block for block in blocks if block != ""]
     return blocks
+
+class BlockType(Enum):
+    HEADINGS = "headings"
+    CODE = "code blocks"
+    QUOTE = "quote blocks"
+    UNORDERED = "unordered list"
+    ORDERED = "ordered list"
+    NORMAL = "normal paragraph"
+
+def block_to_blocktype(md_block: str):
+    if re.match(r"^#{1,6} ", md_block):
+        return BlockType.HEADINGS
+    elif re.match(r"^`{3}\n", md_block) and md_block.endswith("```"):
+        return BlockType.CODE
+    elif all(line.startswith(">") for line in md_block.splitlines()):
+        return BlockType.QUOTE
+    elif all(line.startswith("- ") for line in md_block.splitlines()):
+        return BlockType.UNORDERED
+    elif all(line.startswith(f"{i+1}. ") for i, line in enumerate(md_block.splitlines())):
+        return BlockType.ORDERED
+    else:
+        return BlockType.NORMAL
--- src/test_block_func.py
+++ src/test_block_func.py
@@ -1,5 +1,5 @@
 import unittest
-from block_func import markdown_to_blocks
+from block_func import markdown_to_blocks, BlockType, block_to_blocktype
 
 class TestTextNode(unittest.TestCase):
 # 
@@ -53,6 +53,81 @@ This is a new paragraph on a new line with `code`
             ],
         )
 
+    def test_block_to_type_headings(self):
+        md_block = "# Heading text"
+        block_type = block_to_blocktype(md_block)
+        self.assertEqual(block_type, BlockType.HEADINGS)
+    
+    def test_block_to_type_code(self):
+        md_block ="""```
+        Code block
+        ```"""
+        block_type = block_to_blocktype(md_block)
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_block_to_type_quote1(self):
+        md_block = ">This is a quote block"
+        block_type = block_to_blocktype(md_block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_block_to_type_quote2(self):
+        md_block = "> This is another quote block"
+        block_type = block_to_blocktype(md_block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_block_to_type_quote3(self):
+        md_block ="""> This is another quote block
+> that has multiple
+> quotes"""
+        block_type = block_to_blocktype(md_block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_block_to_type_unordered1(self):
+        md_block = """- Let us
+- write
+- an unordered
+- list"""
+        block_type = block_to_blocktype(md_block)
+        self.assertEqual(block_type, BlockType.UNORDERED)
+
+    def test_block_to_type_unordered2(self):
+        md_block ="""-Let us
+-write
+-an incorrectly formatted
+-unordered list"""
+        block_type = block_to_blocktype(md_block)
+        self.assertNotEqual(block_type, BlockType.UNORDERED)
+
+    def test_block_to_type_ordered1(self):
+        md_block = """1. Let us
+2. write
+3. an ordered
+4. list"""
+        block_type = block_to_blocktype(md_block)
+        self.assertEqual(block_type, BlockType.ORDERED)
+
+    def test_block_to_type_ordered2(self):
+        md_block ="""1. Let us
+3. write
+2. an incorrectly formatted
+4. unordered list"""
+        block_type = block_to_blocktype(md_block)
+        self.assertNotEqual(block_type, BlockType.ORDERED)
+
+    def test_block_to_type_ordered3(self):
+        md_block ="""1.Let us
+2. write
+3. an incorrectly formatted
+4. unordered list"""
+        block_type = block_to_blocktype(md_block)
+        self.assertNotEqual(block_type, BlockType.ORDERED)
+
+    def test_block_to_type_normal(self):
+        md_block = """I am going
+    to write a normal 
+    paragraph block"""
+        block_type = block_to_blocktype(md_block)
+        self.assertEqual(block_type, BlockType.NORMAL)
 
 if __name__ == "__main__":
     unittest.main()
